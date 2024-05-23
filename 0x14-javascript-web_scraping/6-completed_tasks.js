94dfd783ebc6e2227b574563d0a1dec9f8101753
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-23 14:52:29 +0300

    Create 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!completedTasksByUser[task.userId]) {
+        completedTasksByUser[task.userId] = 0;
+      }
+      completedTasksByUser[task.userId] += 1;
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
